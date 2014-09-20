97893e2a3c4df29419b9137719f214fc84bd99f8
unknown <vr222bt@student.lnu.se>
2014-09-20 15:04:29 +0200

    Ritar ut asterisker och byter rad, saknas färg och inskjutning

---
+++ ritamedasterisker/ritamedasterisker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ritamedasterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 25; row++)
+            {
+
+                for (int col = 0; col < 39; col++)
+                {
+                    Console.Write("* ");
+                }
+
+
+
+                Console.WriteLine();
+            }
+
+           
+
+
+
+
+        }
+    }
+}
