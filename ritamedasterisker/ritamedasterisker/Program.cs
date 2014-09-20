eaa40ce46f23dd284f4923490e328f53438435ac
unknown <vr222bt@student.lnu.se>
2014-09-20 21:30:49 +0200

    Lade till färgändring mellan varje rad

--- ritamedasterisker/ritamedasterisker/Program.cs
+++ ritamedasterisker/ritamedasterisker/Program.cs
@@ -13,15 +13,40 @@ namespace ritamedasterisker
             //for loop som körs 25 gånger
             for (int row = 0; row < 25; row++)
             {
+                
+                //if-sats som skriver ut ett mellanslag varannan gång
+                if (row % 2 == 1) 
+                {
+                    Console.Write(" ");
+                }
+
+                //switch-sats som ändrar färgen på asterisken 
+                switch (row % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    
+                }
+
                 //for loop som skirver ut "* " 39 gånger
                 for (int col = 0; col < 39; col++)
                 {
                     Console.Write("* ");
                 }
 
-
+                
                 //Byter rad efter att asteriskerna skrivits ut
                 Console.WriteLine();
+                
             }
 
            
