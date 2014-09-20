ec25e35955498ca2967b868fcb04dda95fc24b7e
unknown <vr222bt@student.lnu.se>
2014-09-20 15:15:03 +0200

    Lade till kommentarer

--- ritamedasterisker/ritamedasterisker/Program.cs
+++ ritamedasterisker/ritamedasterisker/Program.cs
@@ -10,16 +10,17 @@ namespace ritamedasterisker
     {
         static void Main(string[] args)
         {
+            //for loop som körs 25 gånger
             for (int row = 0; row < 25; row++)
             {
-
+                //for loop som skirver ut "* " 39 gånger
                 for (int col = 0; col < 39; col++)
                 {
                     Console.Write("* ");
                 }
 
 
-
+                //Byter rad efter att asteriskerna skrivits ut
                 Console.WriteLine();
             }
 
